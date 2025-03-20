f7514f64aadaab872fac8442832e9d41e744a715
Yan Wirdiny <drawdiny@gmail.com>
2025-03-20 19:10:57 -0400

    skeleton

---
+++ GYMEAL/src/main/java/com/example/GYMEAL/GymealApplication.java
@@ -0,0 +1,13 @@
+package com.example.GYMEAL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymealApplication.class, args);
+	}
+
+}
---
+++ GYMEAL/src/test/java/com/example/GYMEAL/GymealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GYMEAL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
