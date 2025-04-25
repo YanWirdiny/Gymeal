fa2c249159674f4680420a8ece35b8e2f2aeca77
carloscaguana <95885891+carloscaguana@users.noreply.github.com>
2025-04-25 12:44:53 -0400

    Removed filterByCalories() method from 'FilterList' class as well as related tests.

--- GYMEAL/src/main/java/CSV/FilterList.java
+++ GYMEAL/src/main/java/CSV/FilterList.java
@@ -19,27 +19,4 @@ public class FilterList {
     private final int unfilteredListSize = unfilteredFoodList.size();        //size of unfiltered list
 
     //METHODS
-
-    //method that returns a filtered food list based off of a caloric range
-    //this method DOES NOT order them from the largest value to the smallest value or vice-versa.
-    //we can use another method to do that (using QuickSort, RadixSort, etc.)
-    public ArrayList<Food> filterByCalories(int min, int max)
-    {
-        //calories filter
-        Filter<Food> caloriesFilter = new CaloriesFilter(min, max);
-        //list that contains filtered foods based of caloric range
-        ArrayList<Food> filteredList = new ArrayList<>();
-
-        //used to traverse unfiltered food list
-        for(int i = 0; i < unfilteredListSize; i++)
-        {
-            //if food at index i (where 0 <= i < unfilteredListSize) falls withing specified range, add to filtered list
-            if(caloriesFilter.apply(unfilteredFoodList.get(i)))
-            {
-                filteredList.add(unfilteredFoodList.get(i));
-            }
-            //otherwise, do not add the food to the list (do nothing)
-        }
-        return filteredList;
-    }
 }
